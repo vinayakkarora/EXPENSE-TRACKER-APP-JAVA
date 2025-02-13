515ddb150ba77153f9b91db777efcfda852a0b37
Vinayak Arora <150467239+vinayakkarora@users.noreply.github.com>
2025-02-13 14:31:43 +0530

    Update ExpenseTrackerApp.java

--- ExpenseTrackerApp.java
+++ ExpenseTrackerApp.java
@@ -1,4 +1,4 @@
-import org.jfree.chart.ChartFactory
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
